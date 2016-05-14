6fa17fc3f77f6a15dfb5224162d11d01c16b9996
Lucas <lucas@agtlucas.com>
2016-05-14 19:18:15 -0300

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 const gh = require('gh-pages')
 const path = require('path')
 
-gh.publish(path.join(__dirname, '_site'), (err) => {
+gh.publish(path.join(__dirname, ['_site/**/*.*', '_site/CNAME']), (err) => {
   if (err) {
     console.log('Error!', err)
   } else {
