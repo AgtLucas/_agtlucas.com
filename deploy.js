58d23eb159ffeb09a25bed0560a2b7e38bfbd642
Lucas <lucas@agtlucas.com>
2016-07-01 09:54:29 -0300

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,10 +1,14 @@
 const gh = require('gh-pages')
 const path = require('path')
+const chalk = require('chalk')
+
+const error = chalk.bold.red
+const success = chalk.bold.blue
 
 gh.publish(path.join(__dirname, '_site'), (err) => {
   if (err) {
-    console.log('Error!', err)
+    console.log(error('\nError!'), err)
   } else {
-    console.log('Mission accomplished.')
+    console.log(success('\nMission accomplished.'))
   }
 })
